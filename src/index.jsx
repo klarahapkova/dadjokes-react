c71934f044bc4b3c4b6b51be4a2646d82d2a17f7
klarahapkova <80477797+klarahapkova@users.noreply.github.com>
2021-05-22 18:19:08 +0200

    Přidání index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,58 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import { jokes } from './jokes.js';
+import { Joke } from './Joke/Joke';
 
 const App = () => {
-  const [thumbUp, setThumbUp] = useState(0);
-  const [thumbDown, setThumbDown] = useState(0);
-
-  const handleThumbUp = () => {
-    setThumbUp(thumbUp + 1);
-  };
-
-  const handleThumbDown = () => {
-    setThumbDown(thumbDown + 1);
-  };
-
-  return (
-    <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={handleThumbUp}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {thumbUp}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={handleThumbDown}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {thumbDown}
-          </span>
-        </div>
-      </div>
-    </div>
-  );
+  return jokes.map((jokeField) => {
+    return <Joke 
+    key={jokeField.name}
+    name={jokeField.name}
+    avatar={jokeField.avatar}
+    text={jokeField.text}
+    likes={jokeField.likes}
+    dislikes={jokeField.dislikes}
+     />
+  });
 };
 
 render(<App />, document.querySelector('#app'));
